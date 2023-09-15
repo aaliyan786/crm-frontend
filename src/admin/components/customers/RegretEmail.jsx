074bd93853889df6509c26137ea401353c6045ac
sharjeel0288 <k200288@nu.edu.pk>
2023-09-16 01:06:44 +0500

    regret email integration done

--- src/admin/components/customers/RegretEmail.jsx
+++ src/admin/components/customers/RegretEmail.jsx
@@ -1,4 +1,4 @@
-import { React, useState } from "react";
+import React, { useState } from "react";
 import {
   Drawer,
   DrawerOverlay,
@@ -16,63 +16,75 @@ import {
   FormLabel,
   Input,
   Textarea,
+  useToast,
 } from "@chakra-ui/react";
 
 import { ArrowBackIcon } from "@chakra-ui/icons";
+import { FiSend } from "react-icons/fi"; // Import FiSend
 
-import DeleteAlert from "../../../components/common/DeleteAlert"; // Import the DeleteAlert component
-import { BiMailSend, BiSend } from "react-icons/bi";
-import { FiSend } from "react-icons/fi";
+// Assuming these imports are correct
 import { sendRegretEmail } from "../../../API/api";
-const RegretEmail = ({
-  isOpen,
-  onClose,
-  customerDetails,
-  onDelete,
-  onEdit,
-}) => {
+
+const RegretEmail = ({ isOpen, onClose, customerDetails, onSend, onEdit }) => {
   const bgColor = useColorModeValue("gray.100", "gray.700");
   const borderColor = useColorModeValue("gray.200", "gray.600");
-
-  const [isDeleteAlertOpen, setIsDeleteAlertOpen] = useState(false);
-
-  const handleDeleteClick = () => {
-    setIsDeleteAlertOpen(true);
-  };
-
-  const handleConfirmDelete = () => {
-    onDelete(customerDetails);
-    setIsDeleteAlertOpen(false);
-  };
-
   const [description, setdescription] = useState("");
-  const [subject, setSubject] = useState("");
+  const [subject, setSubject] = useState(""); // Add subject state
 
+  const toast = useToast();
   const handleInputChange = (e) => {
-    const inputdescription = e.target.value;
-    setdescription(inputdescription);
+    const { name, value } = e.target;
+    if (name === "description") {
+      setdescription(value);
+    } else if (name === "subject") {
+      setSubject(value); // Update subject state
+    }
   };
 
   const handleSendEmailClick = async () => {
     try {
       const response = await sendRegretEmail(
-        "1", // Replace with actual descriptions
-        "Regret Email",
-        "1",
-        description, // Assuming description is the description
-        "admin123"
+        1, //employee id
+        subject, // Use the subject state
+        customerDetails.id,
+        description,
+        "password" //employee password
       );
 
-      // Handle the response as needed (e.g., show a success description)
       console.log("Email sent successfully:", response);
-
-      // Close the drawer or perform any other action
+      toast({
+        title: "Email Send",
+        description: "The email has been send successfully",
+        status: "success",
+        position: "top-right",
+        duration: 3000,
+        isClosable: true,
+      });
       onClose();
     } catch (error) {
-      // Handle errors (e.g., show an error description)
-      console.error("Email sending error:", error);
+      if (error.response && error.response.data && error.response.data.error) {
+        toast({
+          title: "Error",
+          description: error.response.data.error,
+          status: "error",
+          duration: 3000,
+          position: "top-right",
+          isClosable: true,
+        });
+      } else {
+        toast({
+          title: "Error",
+          description: "Error sending email",
+          status: "error",
+          position: "top-right",
+          duration: 3000,
+          isClosable: true,
+        });
+        console.error("Email sending error:", error);
+      }
     }
   };
+
   return (
     <Drawer isOpen={isOpen} placement="right" onClose={onClose} size="lg">
       <DrawerOverlay />
@@ -124,16 +136,16 @@ const RegretEmail = ({
           >
             <FormControl isRequired>
               <FormLabel>Title.</FormLabel>
-              <Input 
-              type="text" border="1px solid gray"
-              onChange={handleInputChange}
-              value={subject}
-              >
-
-
-              </Input>
-              <FormLabel>description.</FormLabel>
+              <Input
+                type="text"
+                border="1px solid gray"
+                name="subject" // Add a name attribute
+                onChange={handleInputChange}
+                value={subject}
+              />
+              <FormLabel>Description.</FormLabel>
               <Textarea
+                name="description" // Add a name attribute
                 value={description}
                 onChange={handleInputChange}
                 placeholder="Enter description to send."
@@ -142,7 +154,12 @@ const RegretEmail = ({
                 border="1px solid gray"
               />
             </FormControl>
-            <Button variant="solid" colorScheme="green" mt={4}>
+            <Button
+              variant="solid"
+              colorScheme="green"
+              mt={4}
+              onClick={handleSendEmailClick}
+            >
               Send Email <FiSend style={{ marginLeft: "0.4rem" }} />
             </Button>
           </Box>
@@ -153,17 +170,6 @@ const RegretEmail = ({
             Close
           </Button>
         </DrawerFooter>
-
-        {/* Render the DeleteAlert component */}
-        <DeleteAlert
-          isOpen={isDeleteAlertOpen}
-          onClose={() => setIsDeleteAlertOpen(false)}
-          onConfirmDelete={handleConfirmDelete}
-          HeaderText={"Customer Delete"}
-          BodyText={
-            "All the data of this customer will be deleted. Including invoices and qoutations."
-          }
-        />
       </DrawerContent>
     </Drawer>
   );
