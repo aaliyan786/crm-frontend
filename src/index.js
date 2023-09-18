20b755f8df64c849f91139f49b771c5fa6b1128f
ahzam imam <ahzamimam92@gmail.com>
2023-09-18 20:05:38 +0500

    3rd-deployement

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 
 import App from "./App";
 
@@ -10,7 +10,10 @@ const root = createRoot(rootElement);
 root.render(
   <StrictMode>
     <BrowserRouter>
-      <App/>
+      <Routes>
+        <Route path="/" element={<Navigate to="/dashboard" replace />} />
+        <Route path="/dashboard" element={<App />} />
+      </Routes>
     </BrowserRouter>
-  </StrictMode>
+  </StrictMode>,
 );
